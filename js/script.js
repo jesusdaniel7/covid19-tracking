bf2e909b6254b4e37ca2e2bdc2f251a8b43283d7
Jesus Pache <20186837@itla.edu.do>
2020-10-06 12:55:02 -0400

    adding a table countries

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-// fetch('https://www.trackcorona.live/api/countries')
-// .then(response => response.json())
-// .then(response  => {
-//     console.log(response);
-// })
-
-const GetSumary = () => fetch('https://api.covid19api.com/summary');
-const GetCountryDayOne = () => fetch('https://api.covid19api.com/auth/subscriptions');
-
-GetCountryDayOne().then(response => response.json())
+fetch('https://www.trackcorona.live/api/countries')
+.then(response => response.json())
 .then(response  => {
     console.log(response);
 })
 
+const GetSumary = () => fetch('https://api.covid19api.com/summary');
+const GetCountryDayOne = () => fetch('https://api.covid19api.com/auth/subscriptions');
 
+GetSumary().then(response => response.json())
+.then(response  => {
+    console.log(response);
+})
